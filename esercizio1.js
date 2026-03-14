c8024fb27c75ce1eb127b074f3b6b59c417ff8c5
Marco Dispenza <marco.dispenza69@gmail.com>
2026-03-14 15:56:23 +0100

    Aggiunto 2 esercizio

--- esercizio1.js
+++ esercizio1.js
@@ -1,21 +1,36 @@
-// ESERCIZIO 1
+// // ESERCIZIO 1
 
 
-let v = prompt ("inserisci voto")
+// let v = prompt ("inserisci voto")
 
-if (v < 19 && v>=0) {
-    console.log ("insufficente")
+// if (v < 19 && v>=0) {
+//     console.log ("insufficente")
 
+// }
+// else if ( v >=21 && v <24){
+//     console.log ("buono")
+// }
+// else if (v >=24 && v <27){
+//     console.log("distinto")
+// }
+// else if (v >= 27 && v <=29){
+//     console.log("ottimo")
+// }
+//  else if (v ==30){
+//     console.log("eccellente")
+// }else { console.log ("voto non valido")}
+
+
+let t = prompt ("inserisci temperatura")
+
+if  (t < 20 && t >= 0)
+    { console.log ("non ci sono piu le mezze stagioni")}
+else if (t >=30 ){
+    console.log ("lu mare, lu sule e lu ientu")
 }
-else if ( v >=21 && v <24){
-    console.log ("buono")
+else if (t < 0 ) {
+    console.log("non è tanto il freddo quanto l’umidità");
 }
-else if (v >=24 && v <27){
-    console.log("distinto")
-}
-else if (v >= 27 && v <=29){
-    console.log("ottimo")
-}
- else if (v ==30){
-    console.log("eccellente")
+else if (t < -10 ) {
+    console.log ("copriti…ancora ti raffreddi");
 }else { console.log ("voto non valido")}
